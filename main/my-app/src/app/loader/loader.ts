636d3aabf039fefc5a1a0015c8673ef77e7a3e33
jeissonnarvaez-design <jeisson.narvaez@uao.edu.co>
2026-09-09 15:56:43 -0500

    Delete main directory

--- main/my-app/src/app/loader/loader.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loader',
-  standalone: true,
-  templateUrl: './loader.html',
-  styleUrl: './loader.css',
-})
-export class Loader {}
